2e837b391de934e5bb68344f8bbc9b702de3611a
Andrey Lemin <andrey.lemin@dataart.com>
2018-12-10 20:32:41 +0300

    Basic content

---
+++ Source/HelloUnrealWorld/HelloUnrealWorldGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HelloUnrealWorldGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOUNREALWORLD_API AHelloUnrealWorldGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
